aa0e1a6538330438d949aa18b7bc476d02f8b2e2
Madeline Chelangat <113938896+FuryXoXo@users.noreply.github.com>
2022-09-29 23:03:17 +0300

    Add files via upload

---
+++ data/KADERUNAJUNCTION_1.js
@@ -0,0 +1,1 @@
+var json_KADERUNAJUNCTION_1 = {"type":"FeatureCollection","name":"KADERUNAJUNCTION_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"34"},"geometry":{"type":"Point","coordinates":[33.935569992617459,1.118836012619764]}}]}
